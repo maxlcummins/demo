7f4f987298997885f23641f9931a2149beed6192
maxlcummins <max.l.cummins@gmail.com>
2025-10-13 20:25:28 +0100

    update changes

--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@ import ButtonLogin from "@/components/ButtonLogin";
 import FAQListItem from "@/components/FAQListItem";
 import Image from "next/image";
 import productDemo from "./productDemo.jpeg";
+import clientPromise from "@/libs/mongo";
+import { auth } from "@/auth";
 
-export default function Home() {
-  const isLoggedIn = true;
-  const name = "Marc";
+export default async function Home() {
+  const session = await auth();
 
   return (
     <main>
@@ -22,7 +23,7 @@ export default function Home() {
             </a>
           </div>
           <div className="">
-            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+            <ButtonLogin session={session} />
           </div>
         </div>
       </section>
@@ -44,7 +45,7 @@ export default function Home() {
             Create a feedback board in minutes, prioritize features, and build
             products your customers will love.
           </div>
-          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          <ButtonLogin session={session} />
         </div>
       </section>
       {/* PRICING */}
@@ -87,11 +88,7 @@ export default function Home() {
                 );
               })}
             </ul>
-            <ButtonLogin
-              isLoggedIn={isLoggedIn}
-              name={name}
-              extraStyle="w-full"
-            />
+            <ButtonLogin session={session} extraStyle="w-full" />
           </div>
         </div>
       </section>
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,19 +1,39 @@
+"use client";
+
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 // This is a component that renders a login button or a welcome back message
-const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
-  if (isLoggedIn) {
+const ButtonLogin = ({ session, extraStyle }) => {
+  const dashboardUrl = "/dashboard";
+
+  if (session) {
     return (
       <Link
-        href="/dashboard"
-        className={`btn btn-primary + ${extraStyle ? extraStyle : ""}`}
+        href={dashboardUrl}
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
       >
-        Welcome back {name}
+        Welcome back {session.user.name || "friend"}
       </Link>
     );
   }
 
-  return <button className="btn btn-primary">Login</button>;
+  return (
+    <button
+      className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      onClick={() => {
+        signIn(undefined, { callbackUrl: dashboardUrl });
+      }}
+    >
+      Get started
+    </button>
+  );
+
+  // 1. Create a /login page
+
+  // 2. Create an email/password form
+
+  // 3. Make a POST request to /api/auth
 };
 
 export default ButtonLogin;
